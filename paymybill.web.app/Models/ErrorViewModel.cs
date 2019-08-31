019f8b2e080943ce8edc28e4f9a9ad9c7977e9e3
azeem <azeem@gmail.com>
2019-08-31 12:58:10 +0500

    Add project files.

---
+++ paymybill.web.app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace paymybill.web.app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
